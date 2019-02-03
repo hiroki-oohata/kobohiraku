e76fa7ab8424bf5269b306f4fa0db948689b2a21
hiroki-oohata <46782951+hiroki-oohata@users.noreply.github.com>
2019-02-03 20:27:53 +0900

    Delete colorbox.js

--- js/colorbox.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-     $(".iframe").colorbox({iframe:true, width:"350px", height:"300px"});
-  });
-
-  $(".iframe").colorbox({rel:'group1', iframe:true, width:"350px", height:"300px"});
